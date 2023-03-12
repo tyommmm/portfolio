e2abd2346f4fb5c750137a85fe87ee6f0a4b5057
tyommmm <artyomhovsepyan135@gmail.com>
2023-03-12 18:25:29 +0400

    Add: Technologies section, 3D technology balls.

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BallCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { technologies } from '../constants';
 
 const Tech: React.FC = () => {
-  return <div>Tech</div>;
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology: any) => {
+        return (
+          <div className="w-28 h-28" key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
-export default Tech;
+export default SectionWrapper(Tech, '');
